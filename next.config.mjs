5bc7c3c59861e5b3d81427a4ee52602640f6e9b9
Manukrishna J P <themanu110206@gmail.com>
2026-08-27 16:46:04 +0530

    style: fix dark mode class injection and apply Razorpay-like theme

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['127.0.0.1', 'localhost', '192.168.56.1'],
 };
 
 export default nextConfig;
